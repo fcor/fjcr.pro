c3945580afc53e3068bf04c06db66c38986a60b2
Fabio Cortés <fcor14@hotmail.com>
2018-01-19 18:53:52 -0500

    Teaching wireframe ok

--- src/components/Teaching.js
+++ src/components/Teaching.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import animation from '../images/animation.jpg'
+import git from '../images/git.jpg'
 
 const Teaching = () =>
   <div className="contenido">
-    Teaching rendering ok
+    <div className="teaching-content">
+      <Class img={animation}
+        title={"Web animations, Bringing your UIs to life"}
+        description={"In this class, you will learn how to make your users love your website using single web animations. It’s project-oriented with hands-on examples, and will help you with the most fundamental concepts of web animation."}
+      />
+      <Class img={git}
+        title={"Github Essentials"}
+        description={"In this class, I’ll teach you how to get started with git and github, create your own repositories, collaborate with your team and host your own website on github pages!"}
+      />
+    </div>
   </div>
+
+const Class = ({ img, title, description }) =>
+ <div className="clase">
+   <div className="img-box">
+     <img src={img} alt="class img" width="450px"/>
+   </div>
+   <h1>{title}</h1>
+   <p>{description}</p>
+ </div>
 
 export default Teaching
