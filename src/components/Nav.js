94c59a56b98740ed56917962aca1e40073dc63d3
Fabio Cortés <fcor14@hotmail.com>
2018-01-19 17:30:05 -0500

    Random color for navlinks and routes OK

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,42 +3,73 @@ import { NavLink } from 'react-router-dom'
 const colors = ["#C699F7", "#7EBAFF", "#9CEF9D", "#E34B30"]
 
 class Nav extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      color: 'white',
+      navbar: 'Home'
+      //position: 'out'
+    }
+    this.handleHover = this.handleHover.bind(this)
+    this.handleOut = this.handleOut.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
 
   handleHover(e){
-    const col =  colors[Math.floor(Math.random()*colors.length)]
-    e.target.style.color = col
-    // console.log("in");
+    const { navbar } = this.state
+    const nav = e.target.text
+    if (!(navbar === nav)) {
+      const col =  colors[Math.floor(Math.random()*colors.length)]
+      e.target.style.color = col
+    }
   }
+
   handleOut(e){
-    e.target.style.color = "white";
-    // console.log("out");
+    const { navbar } = this.state
+    const nav = e.target.text
+    if (!(navbar === nav)) {
+      e.target.style.color = "white";
+    }
+  }
+
+  handleClick(e){
+    const nav = e.target.text
+    const { navbar } = this.state
+
+    if (!(navbar === nav)) {
+      const col =  colors[Math.floor(Math.random()*colors.length)]
+      // e.target.style.color = col
+      this.setState({
+        navbar: nav,
+        color: col
+      })
+    }
   }
 
   render() {
+    const { color, navbar } = this.state
+    const menu = ["Home", "Projects", "Articles", "Teaching"]
+
     return(
       <nav className="navbar">
-        <ul className='nav'  onMouseOver={(e) => this.handleHover(e)}
-         onMouseOut={(e) => this.handleOut(e)}>
-          <li>
-            <NavLink exact activeClassName='active' to='/'>
-              Home
-            </NavLink>
+        <ul className='nav'>
+          {menu.map( (item) =>
+            <li key={item}
+              onClick={(e) => this.handleClick(e)}
+              onMouseOver={(e) => this.handleHover(e)}
+              onMouseOut={(e) => this.handleOut(e)}
+              >
+              {item === 'Home'
+                ? <NavLink exact to='/' activeStyle={{color: color}} >
+                    {item}
+                  </NavLink>
+                : <NavLink to={`/${item.toLowerCase()}`} activeStyle={{color: color}}>
+                    {item}
+                  </NavLink>
+              }
+
           </li>
-          <li>
-            <NavLink activeClassName='active' to='/projects'>
-              Projects
-            </NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName='active' to='/articles'>
-              Articles
-            </NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName='active' to='/teaching'>
-              Teaching
-            </NavLink>
-          </li>
+           )}
         </ul>
       </nav>
     )
