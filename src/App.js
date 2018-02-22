b16b809ad38ed4dcb68cacb3c594dcf22deabaeb
Fabio Cortés <fcor14@hotmail.com>
2018-02-22 11:43:03 -0500

    Media query support added
    react-media library added. 
    render props setup for component rendering

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import Media from "react-media"
 import Nav from './components/Nav'
 import Home from './components/Home'
 import Teaching from './components/Teaching'
-import './App.css';
+import './App.css'
 
 const App = () =>
   <Router>
-    <div className="react-body">
-      <Nav />
-      <div className="contenido">
-        <Route render={({ location }) =>
-          <TransitionGroup>
-            <CSSTransition key={location.pathname.split('/')[1]} timeout={250} classNames="fade">
-              <Switch location={location}>
-                  <Route exact path="/" component={Home} />
-                  <Route path="/articles" component={Soon}/>
-                  <Route path="/projects" component={Soon}/>
-                  <Route path="/teaching" component={Teaching}/>
-                  <Route render={() => <h1>Page not found</h1>} />
-              </Switch>
-            </CSSTransition>
-          </TransitionGroup>
-        }/>
-      </div>
-      <Footer />
-    </div>
+      <Media query="(min-width: 768px)">
+        { matches =>
+            matches
+              ? <Content />
+              : <h1>Aca ya va responsive</h1>
+        }
+      </Media>
   </Router>
+
+
+const Content = () =>
+  <div className="react-body">
+    <Nav />
+    <div className="contenido">
+      <Route render={({ location }) =>
+        <TransitionGroup>
+          <CSSTransition key={location.pathname.split('/')[1]} timeout={250} classNames="fade">
+            <Switch location={location}>
+                <Route exact path="/" render={() => <Home />} />
+                <Route path="/articles" render={() => <Soon />}/>
+                <Route path="/projects" render={() => <Soon />}/>
+                <Route path="/teaching" render={() => <Teaching />}/>
+                <Route render={() => <h1>Page not found</h1>} />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+      }/>
+    </div>
+    <Footer />
+  </div>
 
 const Footer = () =>
   <footer className="footer">
