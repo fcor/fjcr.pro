e3d0dee71f2e6bec28e6fe9ed8ea8e45d3517f60
Fabio Cortes <fcor14@hotmail.com>
2021-04-07 20:17:22 +0200

    Added new logo and removed old text

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles.css";
 
 import Icon from "../Icon";
-import logo from "../../images/logo.svg";
+import logo2 from "../../images/logo.png";
 
 const iconos = ["github", "twitter", "mail"];
 
@@ -11,22 +11,14 @@ class Home extends React.Component {
     return (
       <div className="home-content">
         <div className="info1">
-          <img src={logo} alt="Logo" />
+          <img src={logo2} alt="Logo" />
         </div>
         <div className="info2">
           <div className="info-texto">
             <h1>Fabio Cortés</h1>
             <h2>I write things that make things.</h2>
             <p>
-              - I'm a WebXR engineer & Creative Coder.
-              <br />
-              <br />
-              - Right now I'm working at the Laboratory for Biomolecular Modeling at EPFL.
-              <br />
-              <br />
-              - I'm learning about 3D modeling and openFrameworks.
-              <br />
-              <br />- I love reading, running, movies, music and beer.
+              &gt; Creative developer exploring the intersection between art and technology.
             </p>
           </div>
           <div className="info-redes">
