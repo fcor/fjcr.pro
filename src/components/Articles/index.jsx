ed911090723f506b8b1c69b9bb836e2e14f80026
Fabio Cortes <fcor14@hotmail.com>
2021-04-08 19:03:37 +0200

    Updated talks and articles sections

--- src/components/Articles/index.jsx
+++ src/components/Articles/index.jsx
@@ -1,19 +1,40 @@
 import React from "react";
 import "./styles.css";
+import { getRandomColor } from "../../utils/index";
 
-const Articles = () => (
-  <div className="articles-container column">
-    <p className="paragraph">I sometimes write about development on my blog:</p>
-    <div className="article column">
-      <h1 className="title">
-        Manipulate your 3D content with gestures in AR.js
-      </h1>
+const Articles = () => {
+  const onHoverOut = (e) => {
+    e.target.style.color = "white";
+  };
+
+  const onHover = (e) => {
+    const col = getRandomColor();
+    e.target.style.color = col;
+  };
+
+  return (
+    <div className="articles-container column">
       <p className="paragraph">
-        Learn how to add gesture controls on any Image tracking or Marker Based
-        AR.js scene.
+        I sometimes write about development on my blog:
       </p>
+      <div className="article column">
+        <a
+          href="https://medium.com/@fabiojcortes/manipulate-your-3d-content-with-gestures-in-ar-js-78da4c076607"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="title"
+          onMouseOver={onHover}
+          onMouseOut={onHoverOut}
+        >
+          Manipulate your 3D content with gestures in AR.js
+        </a>
+        <p className="paragraph">
+          Learn how to add gesture controls on any Image tracking or Marker
+          Based AR.js scene.
+        </p>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Articles;
--- src/components/Talks/index.jsx
+++ src/components/Talks/index.jsx
@@ -1,43 +1,105 @@
 import React from "react";
 import "./styles.css";
+import { getRandomColor } from "../../utils/index";
 
-const Articles = () => (
-  <div className="articles-container column">
-    <p className="paragraph">
-      I often lead talks and workshops about immersive technologies and creative
-      coding:
-    </p>
-    <div className="article column">
-      <h1 className="title">
-        MoleculARweb: Chemistry and Biology education through AR
-      </h1>
-      <p className="paragraph">WebZurich March 2021 meetup - Online</p>
-    </div>
-    <div className="article column">
-      <h1 className="title">UI Design for AR</h1>
-      <p className="paragraph">Global XR Bootcamp 2020 - Online</p>
-    </div>
-    <div className="article column">
-      <h1 className="title">
-        WebXR and the next generation of immersive web experiences
-      </h1>
-      <p className="paragraph">JSWeek 2020 - Online</p>
-    </div>
-    <div className="article column">
-      <h1 className="title">Immersive web experiences with React 360</h1>
-      <p className="paragraph">Boyaconf 2019, Duitama, Colombia</p>
-    </div>
-    <div className="article column">
-      <h1 className="title">From 2D to 3D: UI design for VR</h1>
-      <p className="paragraph">JSConf Colombia 2018, Medellín, Colombia</p>
-    </div>
-    <div className="article column">
-      <h1 className="title">A-Frame & WebVR Workshop</h1>
+const Talks = () => {
+  const onHoverOut = (e) => {
+    e.target.style.color = "white";
+  };
+
+  const onHover = (e) => {
+    const col = getRandomColor();
+    e.target.style.color = col;
+  };
+
+  return (
+    <div className="articles-container column">
       <p className="paragraph">
-        FLISOL Open source festival 2018, Bogotá, Colombia
+        I often lead talks and workshops about immersive technologies and
+        creative coding:
       </p>
+      <div className="article column">
+        <a
+          href="https://youtu.be/yq_APtAkgD8?t=463"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="title"
+          onMouseOver={onHover}
+          onMouseOut={onHoverOut}
+        >
+          MoleculARweb: Chemistry and Biology education through AR
+        </a>
+        <p className="paragraph">WebZurich March 2021 meetup - Online</p>
+      </div>
+      <div className="article column">
+        <a
+          href="https://youtu.be/rzNm_XL4j7w"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="title"
+          onMouseOver={onHover}
+          onMouseOut={onHoverOut}
+        >
+          UI Design for AR
+        </a>
+        <p className="paragraph">Global XR Bootcamp 2020 - Online</p>
+      </div>
+      <div className="article column">
+        <a
+          href="https://youtu.be/9TohHPGviSM?t=3585"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="title"
+          onMouseOver={onHover}
+          onMouseOut={onHoverOut}
+        >
+          WebXR and the next generation of immersive web experiences
+        </a>
+        <p className="paragraph">JSWeek 2020 - Online</p>
+      </div>
+      <div className="article column">
+        <a
+          href="https://2019.boyaconf.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="title"
+          onMouseOver={onHover}
+          onMouseOut={onHoverOut}
+        >
+          Immersive web experiences with React 360
+        </a>
+        <p className="paragraph">Boyaconf 2019, Duitama, Colombia</p>
+      </div>
+      <div className="article column">
+        <a
+          href="https://youtu.be/EsIXYYKJsa0"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="title"
+          onMouseOver={onHover}
+          onMouseOut={onHoverOut}
+        >
+          From 2D to 3D: UI design for VR
+        </a>
+        <p className="paragraph">JSConf Colombia 2018, Medellín, Colombia</p>
+      </div>
+      <div className="article column">
+        <a
+          href="https://flisol.info/FLISOL2019/Colombia/Bogota"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="title"
+          onMouseOver={onHover}
+          onMouseOut={onHoverOut}
+        >
+          A-Frame & WebVR Workshop
+        </a>
+        <p className="paragraph">
+          FLISOL Open source festival 2018, Bogotá, Colombia
+        </p>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
-export default Articles;
+export default Talks;
