9368a77e85821865353b4f19da69e836493d1336
Fabio Cortes <fcor14@hotmail.com>
2021-04-14 11:46:23 +0200

    Repsonsive support to home page

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -17,13 +17,15 @@ const Home = () => {
           <h1 className="title">Fabio Cortés</h1>
           <h2 className="subtitle">I write things that make things.</h2>
           <p className="paragraph">
-            &gt; I'm a Creative developer from Bogotá, Colombia, and now based in Lausanne, Switzerland. 
+            &gt; I'm a Creative developer from Bogotá, Colombia, and now based
+            in Lausanne, Switzerland.
           </p>
           {/* <p className="paragraph">
             &gt; Creative developer exploring the intersection between art and technology. 
           </p> */}
           <p className="paragraph">
-            &gt; I'm focused on immersive technologies, 3D development and generative art. 
+            &gt; I'm focused on immersive technologies, 3D development and
+            generative art.
           </p>
         </div>
         <div className="info-redes">
